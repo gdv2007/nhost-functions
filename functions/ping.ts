a4202a827adf52d6484345efc153240c6fa8ed4f
gdv2007 <davidguillaume2007@gmail.com>
2025-05-09 23:55:46 -0400

    Update ping.ts

--- functions/ping.ts
+++ functions/ping.ts
@@ -1,10 +1,9 @@
 import { Request, Response } from 'express'
 
 export default (req: Request, res: Response) => {
-    res.status(539).json(
-        {
-            headers: req.headers,
-            query: req.query,
-        },
-    )
-}
+    try {
+        res.status(200).json({ success: true });
+    } catch (error) {
+        res.status(500).json({ success: false });
+    }
+};
