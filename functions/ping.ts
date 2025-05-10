10f88b74cf6dd14abcf23318d088a4bcd1fe97e7
gdv2007 <davidguillaume2007@gmail.com>
2025-05-09 23:28:57 -0400

    Update ping.ts

--- functions/ping.ts
+++ functions/ping.ts
@@ -1,6 +1,13 @@
 export default async (req: Request): Promise<Response> => {
-  return new Response(JSON.stringify({ success: true }), {
-    status: 200,
-    headers: { "Content-Type": "application/json" },
-  });
+  try {
+    return new Response(JSON.stringify({ success: true }), {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (error) {
+    return new Response(JSON.stringify({ success: false }), {
+      status: 500,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
 };
