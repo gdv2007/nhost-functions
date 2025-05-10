bbd60a7d6e844c096120824de700c102e78c1f48
gdv2007 <davidguillaume2007@gmail.com>
2025-05-09 20:30:15 -0400

    Add files via upload

---
+++ functions/ping.ts
@@ -0,0 +1,7 @@
+﻿export default () => {
+    try {
+        return { status: 200, body: { success: true } };
+    } catch (error) {
+        return { status: 500, body: { success: false } };
+    }
+};
